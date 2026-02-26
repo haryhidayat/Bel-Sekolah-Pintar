09d736ae1a1b27910eb1c72c72717db83c501ec4
haryhidayat <85499562+haryhidayat@users.noreply.github.com>
2026-02-26 09:25:17 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/Bel-Sekolah-Pintar/',
+})
+
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
